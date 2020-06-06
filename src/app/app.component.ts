602091a5b7bd3892040eece58c61b2f68f725c87
Anton <anton.revta@gmail.com>
2020-06-06 23:46:02 +0300

    how to create stream 4 vid - 17 block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
-import {interval, Subscription} from 'rxjs';
-import {filter, map} from 'rxjs/operators';
+import {Subscription, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,26 +12,37 @@ export class AppComponent {
   переменную мы можем складыывать результат работы метода subscribe()*/
   subscription: Subscription;
 
+  /*чтоб создать свой собственный стрим, мы создаем переменную и помещаем в нее
+  наследника класса Observable библиотеки rxjs. Observable это аналог промиса,
+  и вместо метода resolve у Observable есть аналогичный метод next(). тем самым
+  мы выбрасываем из стрима значение 1, и при подписке это значение выводим в консоль.
+  аналогично методу reject у промисов, у Observable есть метод error, который остановит
+  стрим. эту ошибку мы можем обработать внутри метода subscribe, так как для этого метода
+  второмы необязательным параметром является функция обработки ошибок. третьим
+  необязательным параметром метода subscribe является функция обработки метода complete
+  у экземпляра класса Observable. этот метод завершит стрим. метод complete вызывается
+  только в том случае, если перед моментом его выполнения не произошло никакой ошибки*/
   constructor() {
-
-    /*с помощью ф-и interval() из библиотеки rxjs мы создали новый RxJS-стрим, который
-    запускается каждую секунду. мы в результате получили объект у которого есть метод
-    subscribe(), который выдает нам какой-то результат в случае, если что-то произошло*/
-    const intervalStream$ = interval(1000);
-
-    /*эта переменная отвечает за подписку. у нее есть метод pipe(), который принимает
-    в себя операторы библиотеки rxjs. каждый оператор - ф-я, которая принимает ф-ю, где
-    на каждой итерации мы получаем некоторое значение value, и в теле этой ф-и можем
-    это value изменять. операторы можно чейнить. далее после вызова метода pipe() мы
-    вызываем метод subscribe()*/
-    this.subscription = intervalStream$
-      .pipe(
-        filter(value => value % 2 === 0),
-        map(value => `Mapped value ${value}`)
-      )
-      .subscribe((value) => {
-      console.log(value);
+    const stream$ = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(1);
+      }, 1000);
+      setTimeout(() => {
+        observer.complete();
+      }, 1900);
+      setTimeout(() => {
+        observer.error('Smthn is wrong');
+      }, 2000);
+      setTimeout(() => {
+        observer.next(2);
+      }, 3000);
     });
+    this.subscription = stream$
+      .subscribe(
+        value => console.log('Next: ', value),
+        error => console.log('Error: ', error),
+        () => console.log('complete')
+      );
   }
 
   /*в данном методе мы обращаемся к переменной, отвечающей за подписку, и для того,
