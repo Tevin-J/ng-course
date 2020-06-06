5dbf8298713b566730ce7accdbfcca095ee6c2a5
Anton <anton.revta@gmail.com>
2020-06-06 19:37:57 +0300

    async pipe - block 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+
 export interface IPost {
-  title: string
-  text: string
+  title: string;
+  text: string;
 }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  /*текст инпута*/
-  search: ''
+export class AppComponent implements OnInit{
 
-  /*свойство по которому делаем фильтрацию*/
-  searchField = 'title'
+  /*создаем промис, который через 4 секунды будет резолвисться строкой*/
+  p: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('promise resolved');
+    }, 4000);
+  });
 
-  /*массив постов*/
-  posts: Array<IPost> = [
-    {title: 'beer', text: 'the best beer'},
-    {title: 'bread', text: 'the best bread'},
-    {title: 'javascript', text: 'the best language'},
-  ];
+  date$: Observable<Date> = new Observable(obs => {
+    setInterval(() => {
+      /*в Observable эмиттим новые данные*/
+      obs.next(new Date());
+    }, 1000);
+  });
 
-  addPost() {
-    this.posts.unshift({
-      title: 'new post',
-      text: 'text of new post'
+  /*переменная, которая будет хранить то значение, на которое мы подписались
+  в ngOnInit у Observable*/
+  date: Date
+
+  /*при каждом subscribe получаем новую date и присваиваем ее переменной date. date$
+  пометили символом доллара, так как это стрим, стримы обрабатываются методом subscribe.
+  это сделано для реализации показа Observable без пайпа async*/
+  ngOnInit(): void {
+    this.date$.subscribe(date => {
+      this.date = date;
     });
   }
 }
