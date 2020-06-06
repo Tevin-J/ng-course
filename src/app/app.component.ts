77c04b9942ec7f9a813422ff6adf90094a5bdbfe
Anton <anton.revta@gmail.com>
2020-06-07 00:40:31 +0300

    create stream via Subject class. vid 5 - block 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Subscription, Observable} from 'rxjs';
+import {Subscription, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,42 +12,34 @@ export class AppComponent {
   переменную мы можем складыывать результат работы метода subscribe()*/
   subscription: Subscription;
 
-  /*чтоб создать свой собственный стрим, мы создаем переменную и помещаем в нее
-  наследника класса Observable библиотеки rxjs. Observable это аналог промиса,
-  и вместо метода resolve у Observable есть аналогичный метод next(). тем самым
-  мы выбрасываем из стрима значение 1, и при подписке это значение выводим в консоль.
-  аналогично методу reject у промисов, у Observable есть метод error, который остановит
-  стрим. эту ошибку мы можем обработать внутри метода subscribe, так как для этого метода
-  второмы необязательным параметром является функция обработки ошибок. третьим
-  необязательным параметром метода subscribe является функция обработки метода complete
-  у экземпляра класса Observable. этот метод завершит стрим. метод complete вызывается
-  только в том случае, если перед моментом его выполнения не произошло никакой ошибки*/
+  /*создаем стрим с помощью экземпляра класса Subject библиотеки rxjs. у данного
+  стрима появляется метод next(), позволяющий при его вызове попадать внутрь subscribe*/
+  stream$: Subject<number> = new Subject<number>()
+
+  /*создаем переменную для счетчика, значение которого будем увеличивать при
+  нажатии на кнопку и отрисовывать внутри subscribe*/
+  counter = 0;
+
+  /*в конструкторе подписываем стрим на выполнение ф-и по выводу в консоль значения счетчика*/
   constructor() {
-    const stream$ = new Observable(observer => {
-      setTimeout(() => {
-        observer.next(1);
-      }, 1000);
-      setTimeout(() => {
-        observer.complete();
-      }, 1900);
-      setTimeout(() => {
-        observer.error('Smthn is wrong');
-      }, 2000);
-      setTimeout(() => {
-        observer.next(2);
-      }, 3000);
+    this.subscription = this.stream$.subscribe(value => {
+      console.log('Subscribe', value);
     });
-    this.subscription = stream$
-      .subscribe(
-        value => console.log('Next: ', value),
-        error => console.log('Error: ', error),
-        () => console.log('complete')
-      );
   }
 
   /*в данном методе мы обращаемся к переменной, отвечающей за подписку, и для того,
   чтоб не было утечки памяти, в этом методе мы производим отписку*/
   stop() {
     this.subscription.unsubscribe();
+  }
+
+  /*у стрима, созданного с помощью класса Subject есть метод next(), с помощью
+  которого мы можем автоматически заходить в метод subscribe. мы хотим при нажатии
+  на кнопку в шаблоне выполнять код, который находится в subscribe. то есть вручную
+  эмиттить какие-то события, за которыми будет следить стрим и выполнять действия
+  с теми данными которые мы в subscribe отправим*/
+  myNext() {
+    this.counter++;
+    this.stream$.next(this.counter);
   }
 }
